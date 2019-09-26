32b2e238eb8855c15e7ef4190a03dc8cd14e29d6
macstudent <sandeepjangra0100@gmail.com>
2019-09-25 20:11:17 -0400

    modifiy javamain

--- C0764112_MidTerm_F2019MAD3463/src/main/java/com/Lambton/JavaMain.java
+++ C0764112_MidTerm_F2019MAD3463/src/main/java/com/Lambton/JavaMain.java
@@ -5,7 +5,9 @@ public class JavaMain
     public static void main(String[] args) {
 
         String a="Lambton";
+
         LambtonStringTools obji= new LambtonStringTools();
+
         System.out.println(obji.reverse(a));
 
 
