557c81f0241b98205585db6c5b5a8b94d828a1e4
macstudent <sandeepjangra0100@gmail.com>
2019-09-25 20:11:51 -0400

    modifiy javamain

--- C0764112_MidTerm_F2019MAD3463/src/main/java/com/Lambton/JavaMain.java
+++ C0764112_MidTerm_F2019MAD3463/src/main/java/com/Lambton/JavaMain.java
@@ -2,7 +2,12 @@ package com.Lambton;
 
 public class JavaMain
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+
+
+
+
+    {
 
         String a="Lambton";
 
