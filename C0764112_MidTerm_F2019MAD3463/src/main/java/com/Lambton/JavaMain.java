75b010e77f8298389baa3d537336260e98962719
macstudent <sandeepjangra0100@gmail.com>
2019-09-25 20:14:11 -0400

    modifiy javaclass

--- C0764112_MidTerm_F2019MAD3463/src/main/java/com/Lambton/JavaMain.java
+++ C0764112_MidTerm_F2019MAD3463/src/main/java/com/Lambton/JavaMain.java
@@ -10,16 +10,10 @@ public class JavaMain
         String a="Lambton";
 
         LambtonStringTools obji= new LambtonStringTools();
-
         System.out.println(obji.reverse(a));
 
-
         String Name = "jean luc picard";
-
         LambtonStringTools obj1= new LambtonStringTools();
-
-
-
         System.out.println(obj1.initials(Name));
 
 
